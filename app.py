a4c8aea55460e88a3d7b43ebdf3e69a5cb31659d
Eric Grana <egrana@icloud.com>
2026-04-05 15:13:30 -0700

    - Added secret flask key for session usage
    - Redid venv, removed a lot of requirements that were not being used.
    
    - Added dotenv requirement for env file

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flask import Flask, render_template, request, session, redirect
 
 import APITesting
 from errorValidation import errorCheck
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
 
 @app.route("/")
 def home():
